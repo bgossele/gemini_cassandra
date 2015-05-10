ecc2219dbf6e61f89cc20caf33c52b6a284b81cd
Brecht Gosselé <brecht.gossele@gmail.com>
2015-05-10 21:16:43 +0200

    Catch OperationTimedOut in BatchInserts

--- geminicassandra/gemini_load_chunk.py
+++ geminicassandra/gemini_load_chunk.py
@@ -318,10 +318,22 @@ class GeminiLoader(object):
                         self.write_to_timeoutlog("4::%s;%s;%s;%s;%s\n" % \
                                                 (types_buf[old_i][0], types_buf[old_i][1], types_buf[old_i][2], depth_buf[old_i][2], len(gt_buffer[old_i][2])))
                         
-                except cassandra.OperationTimedOut as e:
-                    self.write_to_timeoutlog("39::%s" % str(e))
-                    stderr.write("Proc %s: OperationTimedOut - goodbye\n")
-                    sys.exit()
+                except cassandra.OperationTimedOut:
+                    if retries[old_i] < retry_threshold:
+                        if retries[old_i] == 0:
+                            self.write_to_timeoutlog("5::%s;%s;%s;%s;%s\n" % \
+                                                (types_buf[old_i][0], types_buf[old_i][1], types_buf[old_i][2], depth_buf[old_i][2], len(gt_buffer[old_i][2])))
+                        future = self.execute_var_gts_batch(types_buf[old_i], depth_buf[old_i], gt_buffer[old_i])      
+                        futures.put_nowait((old_i, future))
+                        retries[old_i] += 1
+                        continue
+                    else:
+                        self.leftover_types.append(types_buf[old_i])
+                        self.leftover_depths.append(depth_buf[old_i])
+                        self.leftover_gts.append(gt_buffer[old_i])
+                        self.write_to_timeoutlog("6::%s;%s;%s;%s;%s\n" % \
+                                                (types_buf[old_i][0], types_buf[old_i][1], types_buf[old_i][2], depth_buf[old_i][2], len(gt_buffer[old_i][2])))
+                        
                 
             future = self.execute_var_gts_batch(types_buf[i], depth_buf[i], gt_buffer[i])
             futures.put_nowait((i, future))
@@ -340,11 +352,11 @@ class GeminiLoader(object):
         try:
             execute_concurrent_with_args(self.session, insert_query, contents)
         except cassandra.WriteTimeout:
-            self.write_to_timeoutlog("5::%d\n" % contents[0][0])   
+            self.write_to_timeoutlog("7::%d\n" % contents[0][0])   
             self.execute_concurrent_with_retry(insert_query, contents, retry)
         except cassandra.InvalidRequest as e:
             if retry < 8:
-                self.write_to_timeoutlog("6::%d\n" % contents[0][0])   
+                self.write_to_timeoutlog("8::%d\n" % contents[0][0])   
                 self.execute_concurrent_with_retry(insert_query, contents, retry+1)
             else:
                 self.write_to_timeoutlog("63::Give up - too many invalid dink\n")
@@ -987,6 +999,8 @@ def load(parser, args):
     if args.anno_type not in ['snpEff', 'VEP', None]:
         parser.print_help()
         exit("\nERROR: Unsupported selection for -t\n")
+        
+    print "Loading starting from %s" % args.offset
 
     # collect of the the add'l annotation files
     annotations.load_annos( args )
