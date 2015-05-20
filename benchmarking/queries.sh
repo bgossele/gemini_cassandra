fd498a1bf99b98ac1540d84bbb82bb15b2206a83
Brecht Gossele <bgossele@lab.exascience.com>
2015-05-20 10:09:27 +0200

    queries.sh

--- benchmarking/queries.sh
+++ benchmarking/queries.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-export db_ips=
+export db_ips=ly-1-00,ly-1-07,ly-2-10,ly-1-12,ly-1-14
 export cores=1
-export keyspace=
+export keyspace=s_db	
 
-geminicassandra query -q "select * from variants \
-                      where sub_type = 'ts' \
-                      and call_rate >= 0.95" -db $db_ips -ks $keyspace
+
+c=1
+n=9
+while [ $c -le $n ] 
+do
+	rm -r exp1_24c_results
+	geminicassandra query -q "select chrom,start,end,pi from variants \
+               	where sub_type = 'ts' \
+        	and call_rate >= 0.95" -db $db_ips -ks $keyspace --cores 24 --exp_id exp1_24c \
+		-batch_size 50
+	((c++))
+done
 
 #geminicassandra query -q "select chrom, start, end, pi from variants" -db $db_ips -ks $keyspace
 
@@ -17,209 +26,209 @@ geminicassandra query -q "select * from variants \
 #                          gts.NA12286 \
 #                   from variants" -db $db_ips -ks $keyspace
 
-#geminicassandra query --header -q "select chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
+#geminicassandra query -q "select chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
 #                            FROM variants LIMIT 50000" -db $db_ips -ks $keyspace
 
-#geminicassandra query --header -q "select chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
+#geminicassandra query -q "select chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
 #                            FROM variants LIMIT 100000" -db $db_ips -ks $keyspace
 
-#geminicassandra query --header -q "select chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
+#geminicassandra query -q "select chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
 #                            FROM variants LIMIT 150000" -db $db_ips -ks $keyspace
 
-#geminicassandra query --header -q "select chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
+#geminicassandra query -q "select chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
 #                            FROM variants LIMIT 200000" -db $db_ips -ks $keyspace
 
-#geminicassandra query --header -q "select chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
+#geminicassandra query -q "select chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
 #                            FROM variants LIMIT 250000" -db $db_ips -ks $keyspace
 
-#geminicassandra query --header -q "select chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
+#geminicassandra query -q "select chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
 #                            FROM variants LIMIT 300000" -db $db_ips -ks $keyspace
 
 #geminicassandra query -q "select chrom, start, end, ref, alt, gene from variants" \
                #--gt-filter "gt_types.HG00239 == HET || gt_types.NA19377 == HOM_REF" \
-               #--header -db $db_ips -ks $keyspace
+               #-db $db_ips -ks $keyspace
 
 #geminicassandra query -q "select chrom, start, end, ref, alt, gene from variants" \
                #--gt-filter "gt_types.HG00239 == HET && gt_types.NA19377 == HOM_REF" \
-               #--header -db $db_ips -ks $keyspace
+               #-db $db_ips -ks $keyspace
 
 #geminicassandra query -q "select chrom, start, end, ref, alt, gene from variants" \
                #--gt-filter "gt_types.HG00239 != HET" \
-               #--header -db $db_ips -ks $keyspace
+               # -db $db_ips -ks $keyspace
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all]" -db $db_ips -ks $keyspace \
 		#--cores 1
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all]" -db $db_ips -ks $keyspace \
 		#--cores 2
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all]" -db $db_ips -ks $keyspace \
 		#--cores 4
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all]" -db $db_ips -ks $keyspace \
 		#--cores 6
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all]" -db $db_ips -ks $keyspace \
 		#--cores 8
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all]" -db $db_ips -ks $keyspace \
 		#--cores 10
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all]" -db $db_ips -ks $keyspace \
 		#--cores 12
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all]" -db $db_ips -ks $keyspace \
 		#--cores 14
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all]" -db $db_ips -ks $keyspace \
 		#--cores 16
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[==HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[>=20].[all]" -db $db_ips -ks $keyspace \
 		#--cores 1
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[==HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[>=20].[all]" -db $db_ips -ks $keyspace \
 		#--cores 2
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter  -db $db_ips -ks $keyspace \
 		#--cores 4
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[==HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[>=20].[all]" -db $db_ips -ks $keyspace \
 		#--cores 6
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[==HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[>=20].[all]" -db $db_ips -ks $keyspace \
 		#--cores 8
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[==HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[>=20].[all]" -db $db_ips -ks $keyspace \
 		#--cores 10
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[==HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[>=20].[all]" -db $db_ips -ks $keyspace \
 		#--cores 12
 
-#geminicassandra query --header \
+#geminicassandra query \ 
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[==HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[>=20].[all]" -db $db_ips -ks $keyspace \
 		#--cores 14
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene, (gts).(phenotype==2) \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[==HOM_REF].[all] &&\
                             #[gt_depths].[phenotype==2].[>=20].[all]" -db $db_ips -ks $keyspace \
 		#--cores 16
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[<10].[count <= 2]" -db $db_ips -ks $keyspace \
 		#--cores 1
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[<10].[count <= 2]" -db $db_ips -ks $keyspace \
 		#--cores 2
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[<10].[count <= 2]" -db $db_ips -ks $keyspace \
 		#--cores 4
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[<10].[count <= 2]" -db $db_ips -ks $keyspace \
 		#--cores 6
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[<10].[count <= 2]" -db $db_ips -ks $keyspace \
 		#--cores 8
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[<10].[count <= 2]" -db $db_ips -ks $keyspace \
 		#--cores 10
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[<10].[count <= 2]" -db $db_ips -ks $keyspace \
 		#--cores 12
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all] && \
                             #[gt_depths].[phenotype==2].[<10].[count <= 2]" -db $db_ips -ks $keyspace \
 		#--cores 14
 
-#geminicassandra query --header \
+#geminicassandra query \
                #-q "SELECT chrom, start, end, ref, alt, gene \
                    #FROM variants" \
                #--gt-filter "[gt_types].[phenotype==2].[!=HOM_REF].[all] && \
